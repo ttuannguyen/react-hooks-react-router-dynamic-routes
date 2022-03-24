8bffe72dbe6baeb1edb1040eb68077c2d5f2cb50
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-03-24 16:25:02 -0400

    commit tree b2513f7c7e64d54d97bbb2dfa2e1f604d5cb2a73

---
+++ src/components/MovieShow.js
@@ -0,0 +1,16 @@
+import react from "react";
+import { useParams } from "react-router-dom";
+
+const MovieShow = ({ movies }) => {
+    //caal useParams to access the 'params' from the url
+    const params = useParams();
+    console.log(params);
+
+    return (
+        <div>
+            <h3>{movies[params.movieId].title}</h3>
+        </div>
+    );
+}
+
+export default MovieShow;
